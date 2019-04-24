173bc38d4f0d2e5ce9eb682d99435b9a05a96e95
Alexander Inman <fujiwara06@gmail.com>
2019-04-23 20:19:07 -0400

    First test. Write class created.

---
+++ PencilKata/Write.cs
@@ -0,0 +1,12 @@
+﻿namespace PencilKata
+{
+    public class Write
+    {
+        public string Written { get; set; }
+
+        public Write(string input)
+        {
+            Written = input;
+        }
+    }
+}
---
+++ PencilKata_Test/Write_Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PencilKata;
+
+namespace PencilKata_Test
+{
+    [TestClass]
+    public class Write_Test
+    {
+        [TestMethod]
+        public void ConstructorShouldAcceptAndReturnString()
+        {
+            Write write = new Write("thing");
+
+            Assert.AreEqual(write.Written, "thing");
+        }
+    }
+}
